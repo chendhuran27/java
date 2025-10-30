af3aec199afd80822785f4063fb51a62a822fa21
chenndhuran <chendhurangeethassrian@gmail.com>
2025-10-30 12:43:23 +0530

    Add files via upload

---
+++ seats.java
@@ -0,0 +1,9 @@
+public class seats {
+    public static void main(String[] args) {
+        String[] names={"harish","chendhuran","charu","badrinath"};
+        int length=names.length;
+        for(int i=0;i<names.length;i++){
+            System.out.println("the seats is "+i+" is taken By " +names[i]);
+        }
+    }
+}
