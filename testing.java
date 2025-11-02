dbea1883ff8898f8c1a21cb770df19bcb6162c18
chenndhuran <chendhurangeethassrian@gmail.com>
2025-11-02 23:16:35 +0530

    Add files via upload

---
+++ testing.java
@@ -0,0 +1,10 @@
+public class testing {
+    public static void main(String[] args) {
+        int[][] numbers={{1,2,3,4},{5,6,7,8,9,10}};
+        for(int[]row:numbers){
+            for(int tierd:row){
+                System.out.println(tierd);
+            }
+        }
+    }
+}
